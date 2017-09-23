8fa656578b1f0c83ef9ddf7863f4b2b66fe64291
alan <alanan46@gmail.com>
2017-09-22 20:57:12 -0400

    new version parser

--- parser/main.java
+++ parser/main.java
@@ -6,7 +6,9 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.FileWriter;
 import java.util.HashMap;
@@ -16,62 +18,78 @@ import java.util.Map;
 
 import java.io.File;
 import java.io.IOException;
-
+import java.util.regex.*;
 public class main {
-	
-	// the get Hidden replies method
-	public static File getHiddenData(String tid,String pid,String pn) throws IOException {
-		//build url
-		String url="http://tieba.baidu.com/p/comment?tid="+tid+"&pid="+pid+"&pn="+pn+"&t=1505875331044";
-		//make connection
-		Document doc = Jsoup.connect(url).get();
-//		Document doc = Jsoup.connect("http://tieba.baidu.com/p/comment?tid=3077857561&pid=51478739271&pn=2&t=1505875331044").get();
-		// output to file and return a File object, here can be changed to return any desired type
-		FileWriter hidden_output=new FileWriter("testHidden.txt");
-		hidden_output.write(doc.toString());
-		hidden_output.close();
-		File hiddenCommentHtml=new File("testHidden.txt");
-		return hiddenCommentHtml;
-	}
-    public static void main(String[] args) throws IOException {
-    	
-    	
-    	
-//         File input = new File("test.html"); 
-    	
-//    		Document doc = Jsoup.parse(input, "UTF-8", ""); 
-    	//make  the function call this way
-    	
-//    		Document doc = Jsoup.parse(getHiddendata("3077857561","51478739271","1"), "UTF-8", ""); 
-        	for (int i=1;i<=2;i++) {
-        		Document doc=Jsoup.parse(getHiddenData("3077857561","51478739271",Integer.toString(i)), "UTF-8", "");
-        		System.out.println(doc);
-        		System.out.println("---------------------------------");
-        		// u can do something like
-        		// parse(doc);  
-        		//which will do parse and write files.
-        	}
-    	
-    		//Document doc = Jsoup.connect("http://tieba.baidu.com/p/4960646551").get(); 
-    	 /*  uncomment this giant block  or maybe put the below into a function so that u can use in the for loop above
-    	 FileWriter output = new FileWriter("result.txt");
-    	 output.write("<dialog>\n");
 
-    	 
-    	 
-    	 Elements post_userID_divs = doc.select("div[class=d_author]"); 
-    	 
-    	 List<String> post_userIDs = new LinkedList<String>();  
-    	 List<String> post_datas = new LinkedList<String>();  
-    	 List<List<String>> reply_datas = new LinkedList<List<String>>();  
-    	 List<List<String>> reply_userIDs = new LinkedList<List<String>>();  
-    	 
+    public static void main(String[] args) throws IOException {
+
+
+    	String url_file_name_prefix = "URLs2";
+    	int save_interval = 50;
+
+    	String url_file_name  = url_file_name_prefix + ".txt";
+
+    	List<String> urls = readFile01(url_file_name);
+    	System.out.println(urls);
+
+    	int i = 0;
+    	int total_dialogue = 0;
+    	int tmp_dialogue = 0;
+
+ 	    FileWriter output = new FileWriter(url_file_name+i+"result.txt");
+ 	 //   FileWriter output = new FileWriter("a.txt");
+
+ 	    output.write("<dialog>\n");
+
+    	for(String url : urls)
+    	{
+    		System.out.println("url #"+i);
+    		tmp_dialogue = parse(url,output);
+    		total_dialogue += tmp_dialogue;
+    		System.out.println("extracted "+tmp_dialogue+" dialogues from this page.");
+    		System.out.println("extracted "+total_dialogue+" dialogues altogether.");
+       	    i++;
+       	    if(i%save_interval == 0)
+       	    {
+           	 output.write("</dialog>");
+        	 output.close();
+        	 System.out.println("save file to "+url_file_name+(i-50)+"result.txt");
+        	 output = new FileWriter(url_file_name+i+"result.txt");
+        	 output.write("<dialog>\n");
+       	    }
+    	}
+
+        output.write("</dialog>");
+   	    output.close();
+   	    return;
+    }
+
+    public static int parse(String url,FileWriter output) throws IOException {
+
+
+     	 Pattern p = Pattern.compile("https*://tieba.baidu.com/p/(\\d+)\\D*");
+    	 Matcher m = p.matcher(url);
+         m.find();
+         String tid = m.group(1);
+
+         System.out.println("url: "+ url);
+
+      //  File input = new File("test.html");
+     	 Document doc = Jsoup.connect(url).get();
+
+    	 Elements post_userID_divs = doc.select("div[class=d_author]");
+
+    	 List<String> post_userIDs = new LinkedList<String>();
+    	 List<String> post_datas = new LinkedList<String>();
+    	 List<List<String>> reply_datas = new LinkedList<List<String>>();
+    	 List<List<String>> reply_userIDs = new LinkedList<List<String>>();
+
     	 post_datas.add("");
-    	 
+
     	 //the comments for the first flow
     	 reply_datas.add(new LinkedList<String>());
     	 reply_userIDs.add(new LinkedList<String>());
-    	 
+
     	 for(Element ele : post_userID_divs)
     	 {
     		 Element li = ele.select("li[class=d_name]").first();
@@ -79,76 +97,129 @@ public class main {
     		 String name = a.text();
     		 if(name =="")
     			 continue;
-    		 System.out.println(name);
     		 post_userIDs.add(name);
     	 }
-    	 
-    	 Elements blocks = doc.select("div[class=d_post_content_main]"); 
-    	 
-    	 System.out.println("name length:"+post_userIDs.size()+" block length:"+blocks.size());
+
+    	 Elements blocks = doc.select("div[class=d_post_content_main]");
+
 
     	 int i = 0;
+    	 int n_dia = 0;
     	 for (Element block : blocks) {
-    		 
+
     		 //This is to see the code of a block which include several comments.
     		 //System.out.println(block);
-    		 
-    		 Element primary_reply = block.getElementsByClass("d_post_content").first();
-        	 Element primary_user_name = block.select("a[alog-group=p_author]").first();
+    		 //System.out.println("-------------------------------------------------");
+    		 Element ele_with_post_data = block.getElementsByClass("d_post_content").first();
 
 
-        	 Elements sub_comments = block.select("span[class=lzl_content_main]");
-        	 Elements user_names = block.select("a[alog-group=p_author]");
-        	 System.out.println("lengths:"+sub_comments.size()+" "+user_names.size());
-        	 System.out.println("Dialogue "+(i+1));
-        	 post_datas.add(primary_reply.text());
-        	 System.out.println(primary_reply.text());
-        	 List<String> temp_comments = new LinkedList<String>();  
-        	 List<String> temp_comment_post_names = new LinkedList<String>();  
-        	 int length = user_names.size();
-        	 for(int j = 0; j < length; j++)
+
+        	 //System.out.println("Dialogue "+(i+1));
+
+        	 post_datas.add(ele_with_post_data.text());
+
+        	 List<String> temp_replys = new LinkedList<String>();
+        	 List<String> temp_reply_userIDs = new LinkedList<String>();
+
+        	 String pid = block.select("a[class=l_post_anchor]").attr("name");
+        	 if(pid!="")
         	 {
-        		 System.out.println(user_names.get(j).text());
-        		 temp_comment_post_names.add(user_names.get(j).text());
-        		 temp_comments.add(sub_comments.get(j).text());
+        		 n_dia++;
+            	 System.out.println("Dialogue "+n_dia);
+        		 int k = 1;
+        		 while(true)
+        		 {
+            		 Document reply_doc = getHiddenData(tid,pid,Integer.toString(k));
+            		 //System.out.println(reply_doc);
+            		 k++;
+
+            		 Elements replys = reply_doc.getElementsByClass("lzl_single_post");
+            		 if(replys.size()==0)
+            		 {
+            			 break;
+            		 }
+            		 for(Element reply:replys)
+            		 {
+            			 temp_reply_userIDs.add(reply.select("a[class=at j_user_card ]").first().text());
+            			 //System.out.println("reply user id:"+reply.select("a[class=at j_user_card ]").first().text());
+            			 temp_replys.add(reply.select("span[class=lzl_content_main]").first().text());
+            			 //System.out.println("reply content:"+reply.select("span[class=lzl_content_main]").first().text());
+            		 }
+        		 }
         	 }
-        	 reply_datas.add(temp_comments);
-        	 reply_userIDs.add(temp_comment_post_names);
+
+
+        	 reply_datas.add(temp_replys);
+        	 reply_userIDs.add(temp_reply_userIDs);
     		 i += 1;
     		 }
-    	 System.out.println(post_userIDs.size()+" "+post_datas.size()+" "+ reply_datas.size()+" "+reply_userIDs.size());
-    	 
-    	 for(i=1;i<post_userIDs.size();i++)
+
+    	 // Those four lists should have the same size which is the number of posts in a page.
+    	 System.out.println("post_userIDs.size:"+post_userIDs.size()+" post_datas.size:"+post_datas.size()+" reply_datas.size:"+ reply_datas.size()+" reply_userIDs.size:"+reply_userIDs.size());
+
+    	 int n_post = post_userIDs.size();
+
+    	 //Dump the extracted data into the result file in the required format.
+    	 // i begins from 1 since the first post doesn't have a reply. In fact, other posts are all reply to the first one but we don't take any post together with the first post as a dialogue yet.
+    	 for(i=1;i<n_post;i++)
     	 {
-    		 Map name_map = new HashMap();
-    		 
-    		 name_map.put(post_userIDs.get(i), 1);
-    		 int k = 2;
-    		 for(String name : reply_userIDs.get(i))
-    		 {
-    			 if(!name_map.containsKey(name))
-    			 {
-    				 name_map.put(name, k);
-    				 k++;
-    			 }
-    		 }
-    		 
+
     		 if(reply_datas.get(i).size()>0)
     		 {
+
+        		 // maps names to numbers
+        		 Map name_map = new HashMap();
+
+        		 name_map.put(post_userIDs.get(i), 1);
+        		 int k = 2;
+        		 for(String name : reply_userIDs.get(i))
+        		 {
+        			 if(!name_map.containsKey(name))
+        			 {
+        				 name_map.put(name, k);
+        				 k++;
+        			 }
+        		 }
+
     			 output.write("<s>");
-    			 output.write("<utt uid=\""+name_map.get(post_userIDs.get(i))+"\">"+post_datas.get(i)+"</utt>");
+    			 output.write("<utt uid=\""+name_map.get(post_userIDs.get(i)).toString().replaceAll("[<>]","")+"\" "+" uname=\""+post_userIDs.get(i).toString().replaceAll("[<>]","")+"\" >"+post_datas.get(i).toString().replaceAll("[<>]","")+"</utt>");
     			 int n_comments = reply_datas.get(i).size();
     			 for(int j = 0;j < n_comments;j++)
     			 {
-        			 output.write("<utt uid=\""+name_map.get(reply_userIDs.get(i).get(j))+"\">"+reply_datas.get(i).get(j)+"</utt>");
+        			 output.write("<utt uid=\""+name_map.get(reply_userIDs.get(i).get(j)).toString().replaceAll("[<>]","")+"\" "+" uname=\""+reply_userIDs.get(i).get(j).toString().replaceAll("[<>]","")+"\" >"+reply_datas.get(i).get(j).toString().replaceAll("[<>]","")+"</utt>");
     			 }
     			 output.write("</s>\n");
     		 }
     	 }
-    	 output.write("</dialog>");
-    	 output.close();
-    	*/
+    	 return n_dia;
     }
-    
-}
 
+    public static List<String> readFile01(String file_name) throws IOException {
+        FileReader fr=new FileReader(file_name);
+        BufferedReader br=new BufferedReader(fr);
+        String line="";
+        String[] arrs=null;
+        List<String> result = new LinkedList<String>();
+
+        //For the first line
+        //line=br.readLine();
+        while ((line=br.readLine())!=null) {
+        	result.add(line);
+        }
+        br.close();
+        fr.close();
+        return result;
+    }
+
+	public static Document getHiddenData(String tid,String pid,String pn) throws IOException {
+		//build url
+		String url="http://tieba.baidu.com/p/comment?tid="+tid+"&pid="+pid+"&pn="+pn+"&t=1505875331044";
+		System.out.println("reply url:"+url);
+		//make connection
+		Document doc = Jsoup.connect(url).get();
+//		Document doc = Jsoup.connect("http://tieba.baidu.com/p/comment?tid=3077857561&pid=51478739271&pn=2&t=1505875331044").get();
+		// output to file and return a File object, here can be changed to return any desired type
+		return doc;
+	}
+
+}
